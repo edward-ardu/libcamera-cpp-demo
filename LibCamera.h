1c23258eea3d76b7e38f481acb189aa096cdb74d
edward-ardu <113565969+edward-ardu@users.noreply.github.com>
2022-10-14 14:13:28 +0800

    Add files via upload

---
+++ LibCamera.h
@@ -0,0 +1,76 @@
+#include <atomic>
+#include <iomanip>
+#include <iostream>
+#include <signal.h>
+#include <limits.h>
+#include <memory>
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <sstream>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <time.h>
+#include <mutex>
+
+#include <libcamera/controls.h>
+#include <libcamera/control_ids.h>
+#include <libcamera/property_ids.h>
+#include <libcamera/libcamera.h>
+#include <libcamera/camera.h>
+#include <libcamera/camera_manager.h>
+#include <libcamera/framebuffer_allocator.h>
+#include <libcamera/request.h>
+#include <libcamera/stream.h>
+#include <libcamera/formats.h>
+#include <libcamera/transform.h>
+
+using namespace libcamera;
+
+typedef struct {
+    uint8_t *imageData;
+    uint32_t size;
+    uint64_t request;
+} LibcameraOutData;
+
+class LibCamera {
+    public:
+        LibCamera(){};
+        ~LibCamera(){};
+        
+        int initCamera(const StreamRoles &role, int width, int height, PixelFormat format, int buffercount, int rotation);
+        
+        int startCamera();
+        bool readFrame(LibcameraOutData *frameData);
+        void returnFrameBuffer(LibcameraOutData frameData);
+
+        void set(ControlList controls);
+        void stopCamera();
+        void closeCamera();
+    private:
+        int startCapture();
+        int queueRequest(Request *request);
+        void requestComplete(Request *request);
+        void processRequest(Request *request);
+
+        unsigned int cameraIndex_;
+	    uint64_t last_;
+        std::unique_ptr<CameraManager> cm;
+        std::shared_ptr<Camera> camera_;
+        bool camera_acquired_ = false;
+        bool camera_started_ = false;
+	    std::unique_ptr<CameraConfiguration> config_;
+        std::unique_ptr<FrameBufferAllocator> allocator_;
+        std::vector<std::unique_ptr<Request>> requests_;
+        // std::map<std::string, Stream *> stream_;
+        std::map<int, std::pair<void *, unsigned int>> mappedBuffers_;
+
+        std::queue<Request *> requestQueue;
+
+        ControlList controls_;
+        std::mutex control_mutex_;
+        std::mutex camera_stop_mutex_;
+        std::mutex free_requests_mutex_;
+};
---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <chrono>
+
+#include "LibCamera.h"
+
+int writeToFile(const char *fileName, uint8_t *buffer, uint32_t bufferSize ) {
+    FILE* outF;
+    outF = fopen(fileName, "wb");
+
+    if( outF == NULL )
+    {
+        return 1;
+    }
+
+    if( fwrite(buffer, 1, bufferSize, outF) < 0 )
+    {
+        return 2;
+    }
+
+    fclose(outF);
+
+    return 0;
+}
+
+int main() {
+    time_t start_time = time(0);
+    int frame_count = 0;
+    LibCamera cam;
+    int width = 640;
+    int height = 480;
+    char key;
+
+    //int ret = cam.initCamera( {StreamRole::Viewfinder}, 1280, 720, formats::RGB888, 1, 0 ); // Test = AF not working too !
+    //int ret = cam.initCamera( {StreamRole::Viewfinder}, 9152, 6944, formats::RGB888, 1, 0 ); // Test = AF not working too !
+    int ret = cam.initCamera( {StreamRole::Raw}, 9152, 6944, formats::SRGGB10_CSI2P, 1, 0 ); // AF not working !
+
+    ControlList controls_;
+    int64_t frame_time = 1000000 / 30;
+    // Set frame rate
+	controls_.set(controls::FrameDurationLimits, libcamera::Span<const int64_t, 2>({ frame_time, frame_time }));
+    // Adjust the brightness of the output images, in the range -1.0 to 1.0
+    controls_.set(controls::Brightness, 0.5);
+    // Adjust the contrast of the output image, where 1.0 = normal contrast
+    controls_.set(controls::Contrast, 1.5);
+    // Set the exposure time
+    controls_.set(controls::ExposureTime, 20000);
+    cam.set(controls_);
+
+    if (!ret) {
+        bool flag, end;
+        LibcameraOutData frameData;
+        cam.startCamera();
+        end = false;
+        while (!end) {
+            // Wait a frame
+            flag = cam.readFrame(&frameData);
+            if (!flag)
+                continue;
+            frame_count++;
+
+            // Force autofocus every 3 frames  
+            if( frame_count % 3 == 0) {
+                controls_.set(controls::AfTrigger, 1);
+                cam.set(controls_);
+                std::cout << "AE" << std::endl;
+            }
+
+            // Save the 20th frame
+            if( frame_count >= 20 ) {
+                ret = writeToFile( "../frame.dat", frameData.imageData, frameData.size );
+                if( ret )
+                {
+                    std::cerr << "Failed to write to file ../frame.dat #" << ret << std::endl;
+                    return 1;
+                }
+                end = true;
+                std::cout << "END" << std::endl;
+            }
+
+            cam.returnFrameBuffer(frameData);
+        }
+        cam.stopCamera();
+    }
+    cam.closeCamera();
+    return 0;
+}
